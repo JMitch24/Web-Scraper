3266b127c84025e600f5a8d61247b22b54e0ac32
Jesse Mitchell <jmitchell24@biomedscienceacademy.org>
2020-12-11 18:12:00 +0000

    I HAVE PRODUCED RESULTS!! Can now pull main feture articles featured Headline and now I know how to understand html as painful of a journey as it was haha but now I know how to recreate the results I got and in theory apply it to all of the articles

--- main.py
+++ main.py
@@ -5,6 +5,11 @@ import requests
 # Big thank you to SixBeeps(3265) who helped me out with figuring out how to import beautifulsoup module. Repl Forrum Disscussion that helped me: https://repl.it/talk/ask/How-do-i-download-beautifulsoup-to-my-repl/27035.  
 topic_selection = input("What topics are ya\' lookin\' for today for today?: ")
 
-url = "https://www.politico.com/congress"+topic_selection
+url = "https://www.politico.com/"+topic_selection
 page = requests.get(url)
 
+soup= BeautifulSoup(page.content, 'lxml')
+article = soup.find('div', class_= 'summary')
+
+headline = article.h3.a.text
+print(headline)
