38e6416b6a711318eed438f2124a3d1e08c15a67
Jesse Mitchell <jmitchell24@biomedscienceacademy.org>
2020-12-18 21:32:24 +0000

    Final Version 1 of the code. All headlines and summaries parse for all media sources. And the code successfully works after many test with the inputs, 'congress' or 'white-house'

--- README.md
+++ README.md
@@ -4,38 +4,38 @@ The purpose of this bit of code and program is to scrape infromation and news fr
 
 ## Description
 
-An in-depth paragraph about your project and overview of use.
+This code and project was desined to serve a goal of making it easier and faster to consume news and specifically political content. Ultimetly this program will the users input which currently can only be "congress" or "white-house" to then find articles and news relating to those subjects. The infromation of were diffrent articles or podcasts are coming from but they are Politico, NPR, and Washington Post. With the help of the BeauftifulSoup module the code will pull the either top 2 or 1 articles and it'll give the user the headline, summary, and link to the sources. This proram is by no means supsosed to be used as a sinular resource and articles should be bread on their respective sites. This code and program just makes it easier for the user to know what's trending and going on and how to get to it! 
+
 
 ## Getting Started
 
 ### Dependencies
 
-* Describe any prerequisites, libraries, OS version, etc., needed before installing program.
-* ex. Windows 10
-
-The Follwoing List of Modules were Used and Would be Needed to Run the Code:
-
+The following list of modules were used and would be needed to run the code:
+* Termcolor
+* Requests 
+* BeauftifulSoup
 
 
 ### Installing
-
-* How/where to download your program
-* Any modifications needed to be made to files/folders
-
+* There are no downloads, fies, folders that need to be downloaded. Here in Repl the code is ready to go and if you hit Run within main.py everything should work, no downloads required :D
 ### Executing program
+To run the program:
 
+1. When you first run and start the code it will prompt you for some input. With the way the code is in it's current state the only inputs that'll work and not end in an error are: "congress" and "white-house"
+2. After entering and submistting your input the program will begin to work printing out a couple of Articles with headlines and summaries. The first two will be from Politco, the middle two will be from NPR, and the bottom one will be from Washington Post
+3. After the time of giving your input wait at least 5 to 15 seconds for Washington Posts Articel Parsing takes a good coupple of seconds. It will look like the code is doing nothing but just wait it should print it. 
+4. Look at the numbers that will look like this: "[1]" next to the Bodled Article Headlines. Read throught the summaries and headlines and find the article that intrests you the most.
+5. Then after the last article is parsed and prints you the user will be prompted to give input on the article you wanted to read and see. Input just the corresponding number of the article in and the program will output a link to the article you wanted to see.
+6. And That's All Folks! 
 * How to run the program
 * Step-by-step bullets
-```
-code blocks for commands
-```
+
 
 ## Help
 
-Any advise for common problems or issues.
-```
-command to run if program contains helper info
-```
+If issues are run into with the code the best suggestion is to re-run it making sure to double check your first intial inputs spelling and formating, and that you are inputing a valid article number on the second input. If isssues still arise after resarting the page you the contact bellow so I can check out the code and see if at all there are issues I can fix :)
+
 
 ## Authors
 * Jesse Mitchell
@@ -45,21 +45,28 @@ command to run if program contains helper info
 
 ## Version History
 
-* 0.2
-    * Various bug fixes and optimizations
-    * See [commit change]() or See [release history]()
+
 * 0.1
-    * Initial Release
+    * Initial Pubic Release 2020 12 18
 
-## License
 
-This project is licensed under the [NAME HERE] License - see the LICENSE.md file for details
 
 ## Acknowledgments
 
-Inspiration, code snippets, etc.
-* [awesome-readme](https://github.com/matiassingers/awesome-readme)
-* [PurpleBooth](https://gist.github.com/PurpleBooth/109311bb0361f32d87a2)
-* [dbader](https://github.com/dbader/readme-template)
-* [zenorocha](https://gist.github.com/zenorocha/4526327)
-* [fvcproductions](https://gist.github.com/fvcproductions/1bfc2d4aecb01a834b46)
+Just a few of the amazing people that helped make this possible: 
+* [DomPizzie](https://gist.github.com/DomPizzie/7a5ff55ffa9081f2de27c315f5018afc)
+* [Washington Post](https://www.washingtonpost.com/)
+* [Politico](https://www.politico.com/)
+* [NPR](https://www.npr.org/)
+* [Allen O'Neill](https://www.pluralsight.com/guides/guide-scraping-media-from-the-web-python)
+* [Miguel Fernández Zafra](https://towardsdatascience.com/web-scraping-news-articles-in-python-9dd605799558)
+* [Zuko](https://stackoverflow.com/questions/8924173/how-do-i-print-bold-text-in-python)
+* [Gareth Dwyer](https://www.codementor.io/@garethdwyer/beginner-web-scraping-with-python-and-repl-it-nzr27jvnq)
+* [Leodanis Pozo Ramos](https://realpython.com/python-return-statement/)
+* [freeCodeCamp.org](https://www.youtube.com/watch?v=XVv6mJpFOb0&list=PL279jbUfgAWhhdLc4PXz8zdFxRDsKV0xT&index=3&t=2706s)
+* [Corey Schafer](https://www.youtube.com/watch?v=ng2o98k983k&t=2100s)
+* [Traversy Media](https://www.youtube.com/watch?v=4UcqECQe5Kc&t=192s)
+* [Dev Ed](https://www.youtube.com/watch?v=Bg9r_yLk7VY&t=320s)
+* [Keith Galli](https://www.youtube.com/watch?v=GjKQ6V_ViQE&t=313s)
+* [SixBeeps3265](https://repl.it/talk/ask/How-do-i-download-beautifulsoup-to-my-repl/27035)
+
--- main.py
+++ main.py
@@ -1,9 +1,8 @@
+#This part of the code installs the nessacariy modules and libraries it needs to run
 import requests
 from bs4 import BeautifulSoup
-
 from termcolor import colored
-
-
+# This function below create a little decreative line to go inbettwen the diffrent News Source parsings. It returns a 40 long line pattern of "-_" when called. 
 def dividing_line():
     x = 0
     while x < (100):
@@ -11,58 +10,50 @@ def dividing_line():
         x = x + 5
 
 
-# Big thank you to SixBeeps(3265) who helped me out with figuring out how to import beautifulsoup module. Repl Forrum Disscussion that helped me: https://repl.it/talk/ask/How-do-i-download-beautifulsoup-to-my-repl/27035.
-
+#This part of the code is the maininput from the user. It gets the users topic selection which with the code at it's current state destides between "congress" or "white-house". It reurtns the user's input in topic_selection which can gget tagged along at the end of url commands to find that specific news page based off of that topic. 
 topic_selection = input("What topics are ya\' lookin\' for today for today?: ")
 print("\n")
 
 dividing_line()
 print("Politco Articles: ")
-
+#This code below crates the url form politico's base url plus it's adition /tags which topic_selection perfectly solted into. In the end cretes the url to Politco's Congrees or White House sections. 
 url = "https://www.politico.com/" + topic_selection
 page = requests.get(url)
 
 soup = BeautifulSoup(page.content, 'lxml')
 
 
-
+#The varible article below is what holds the parsed html from the website. 
 article = soup.find('div', class_='summary')
-
+#In the code blow headline searches throught the html stored in 'article' and finds the first h3 then a tag which it then removes its formating and outputs stright text
 headline = article.h3.a.text
 print("\n[0] \t" + colored(headline, attrs=['bold']))
-
+#Below is where an article's summary is found and prased from 'article' defined above.
 summary = article.p.text
 print("\n" + summary)
+#Below this code parse the href links brurrowed within a tags apart of the Article's headline and summary. 
 article_link = article.h3.a
 for link in article_link:
   href = article_link.get("href")
-
 politico_zero_href = href
 
 
 
-
+#This code section below works very siliar to that of the one above it. However what's unique about this block of code is that it parses from a diffrent location within url which allows for two articles to be parsed without findAll() and without the same reults being repeated. 
 article = soup.find('div', class_='content-group tag-latest')
-
 headline = article.h3.a.text
 print(colored("\n [1] \t" + headline, attrs=['bold']))
-
 article_link = article.h3.a
 for link in article_link:
   href = article_link.get("href")
-
 url = (href)
 page = requests.get(url)
 soup = BeautifulSoup(page.content, 'lxml')
+#With the way html works something I never knew was the classifiactions that diffrent parts of it get. So below 'dek' is a class in which more information is stored underneth it but it was unuqie enough I could parse it wihout having to break it down furter. Then the 'p'stnads for the parargaph text identification the part of the code we want has on their website. 
 article = soup.find('p', class_='dek')
 summary = article.text
 print("\n" + summary)
 politico_one_href = href
-
-
-
-
-
 
 
 dividing_line()
@@ -70,18 +61,16 @@ print("NPR Daily Podcasts: ")
 
 
 
+#These next two chuncks of code below are the ones responsible for parrsing the urls to their audio recordings for their respecitive topics. The code directly below parses information from NPR Politics Podcast.
 url = 'https://www.npr.org/podcasts/510310/npr-politics-podcast'
 page = requests.get(url)
-
 soup = BeautifulSoup(page.content, 'lxml')
 article = soup.find('div', class_='item-info')
-
 headline = article.h2.a.text
+#Below the colored(headline, attrs=['bold'])) code is a super amazing text module and library I found that allows for text to be easily bolded which allows the article titles to stand out. 
 print("\n [2] \t" + colored(headline, attrs=['bold']))
-
 summary = article.p.text
 print(summary)
-
 article_link = article.h2.a
 for link in article_link:
   href = article_link.get("href")
@@ -89,49 +78,40 @@ npr_one_href = (href)
   
 
 
-
+#This code bit beneath is what parse the most recent podcast headline and summary headline from NPR's own Up First podcast. 
 url = 'https://www.npr.org/podcasts/510318/up-first'
 page = requests.get(url)
-
 soup = BeautifulSoup(page.content, 'lxml')
 article = soup.find('div', class_='item-info')
-
+#What's super amazing and diffrent about the way NPR has their html set up. Within thier basic h2 and a tags as seen below is the Date of the release of the Podcast. My intial reaction was how quickly can I get rid of it but I kept it because the other two media sources are hard artticles and news that update more reguarly. But Podcasts are more spratic and distanced in times when they come out so it was left it to be a reference. 
 headline = article.h2.a.text
 print("\n [3] \t" + colored(headline, attrs=['bold']))
-
 summary = article.p.text
 print(summary)
-
 article_link = article.h2.a
 for link in article_link:
   href = article_link.get("href")
 npr_two_href = (href)
- 
-
-
-
+#
 
 dividing_line()
 print("Washington Post Articles: ")
 
-
+#Below in this next section of code is where the Waashinton Post's new's article is parsed from.
+#Washington Post has their url and liks set up so weirdly that their 'congress' tab is actually called 'powerpost' so a create if else statment was needed as seen below to translate it to useable terms for the code and website. 
 if topic_selection == "congress":
     topic_selection = "powerpost"
 else:
     topic_selection = topic_selection
-
 url = "https://www.washingtonpost.com/politics/" + topic_selection
 page = requests.get(url)
-
 soup = BeautifulSoup(page.content, 'lxml')
 article = soup.find('div', class_='story-headline')
 headline = article.h2.a.text
 print("\n [4] \t" + colored(headline, attrs=['bold']))
-
 article_link = article.h2.a
 for link in article_link:
   href = article_link.get("href")
-
 article = soup.find('div', class_='story-description')
 summary = article.p.text
 print("\n" + summary)
@@ -139,27 +119,16 @@ wp_one_href = (href)
 
 
 
-
-
 dividing_line()
-
+#This code below is the what takes the user's input form the input question below and outputs the corresponding link to the article the user wanted to see.
 user_article_choice =input("\n Which of the Articles Above do ya\' want to see: ")
-
+#These if/else statments match up the links outut by each of the code sections above with the article headlines numerical value that was assigned to them
 if user_article_choice == "0":
   article_url = politico_zero_href
-  page = requests.get(article_url)
-  soup = BeautifulSoup(page.content, 'lxml')
-  article = soup.find('div', class_='story-text')
-  print(article.text)
-  
-  
-
+  print(article_url)
 elif user_article_choice == "1":
   article_url = politico_one_href
-  page = requests.get(article_url)
-  soup = BeautifulSoup(page.content, 'lxml')
-  article = soup.find('div', class_='container__column container__column--story center-horizontally')
-  print(article.text)
+  print(article_url)
 elif user_article_choice == "2":
   article_url = npr_one_href 
   print(article_url)
@@ -171,7 +140,6 @@ elif user_article_choice == "4":
   print(article_url) 
 else:
   print("Sorry that's not a valid article number to chose from.")
-
 
 
 
